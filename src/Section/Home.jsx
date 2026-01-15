f1d8e6e7a8ec51a5f61e1d6669e13ccacfdfcb1a
Krishnakumar Kashi Arjun <arjunkrishnakumar90@gmail.com>
2026-01-15 23:18:13 +0530

    Update package files and sections

--- src/Section/Home.jsx
+++ src/Section/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="home-section" id='home'>
-      <img src="/img.png" alt="Home Banner" />
+      <img src={`${process.env.PUBLIC_URL}/img.png`} alt="Home Banner" />
       <span>Hello!</span>
       <p>I'm <strong>Krishnakumar Arjun</strong> - Computer Science Student and a Passionate Web Developer. </p>
     </div>
--- src/Section/Project.jsx
+++ src/Section/Project.jsx
@@ -9,17 +9,17 @@ const Project = () => {
         <Card
             title="Personal Portfolio Website"
             description="A personal portfolio website built with React to showcase my projects and skills."
-            image="/proj-img1.jpg"
+            image={`${process.env.PUBLIC_URL}/proj-img1.jpg`}
         />
         <Card   
             title="E-commerce Store"
             description="An online store built with Node.js and Express, featuring user authentication and payment integration."
-            image="/proj-img1.jpg"
+            image={`${process.env.PUBLIC_URL}/proj-img1.jpg`}
         />
         <Card
             title="Chat Application"
             description="A real-time chat application using Socket.io and React for seamless communication."
-            image="/proj-img1.jpg"
+            image={`${process.env.PUBLIC_URL}/proj-img1.jpg`}
         />
     </div>
     </div>
